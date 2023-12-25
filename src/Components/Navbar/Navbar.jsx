17a5d6765ea001a8260c16630e75d7f262a9783d
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-25 21:51:41 +0600

    sdv

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Nav.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="n-main">
+                <div className="n-left">
+                    <div className="n-name">
+                        Juwel
+                    </div>
+                    <span>Toggle</span>
+                </div>
+                <div className="n-right">
+                    <div className="n-list">
+                        <ul>
+                            <li>Home</li>
+                            <li>Services</li>
+                            <li>Experience</li>
+                            <li>Protfolio</li>
+                            <li>Testimonials</li>
+                        </ul>
+                        <button className='button'>Contact <br></br> me</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
