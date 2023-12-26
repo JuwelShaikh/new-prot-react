93c2daf90d0b77cf32c2a04ebda63c9fc212460b
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-26 15:39:33 +0600

    intro compo added

---
+++ src/Components/intro/Intro.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Intro = () => {
+    return (
+        <div className='intro'>
+            <div className="i-left">
+                <div className="i-name">
+                    <span>Hi, I am</span>
+                    <span>Juwel Shaikh</span>
+                    <span>JR Frontend Developer with high level of creativity and confidence. It should give quality work </span>
+                </div>
+                <button>Hire me!</button>
+            </div>
+            <div className="i-right"></div>
+            
+        </div>
+    );
+};
+
+export default Intro;
