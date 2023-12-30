70cd2113d06f17291efd25cdd8e352f42401d57b
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-30 12:29:54 +0600

    Divs added

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Services = () => {
     return (
         <div className='services'>
+            {/* left-side */}
+        <div className="awesome">
+
+        </div>
+        {/* right-side */}
+        <div className="cards">
             
+        </div>
         </div>
     );
 };
