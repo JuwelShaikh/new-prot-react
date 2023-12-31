9c8ca8b3f447a8807d2f705cf1bddf8aeaa28952
JuwelShaikh <juwelshaikh04@gmail.com>
2023-12-31 14:08:56 +0600

    Experience done

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -17,6 +17,7 @@ const Experience = () => {
             <div className="achievement">
                 <div className="circle">5+</div>
                 <span>Companies</span>
+                <span>I have worked</span>
                 
             </div>
             
