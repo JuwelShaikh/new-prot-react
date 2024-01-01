8fff58ca9597b08f2dac76ff03c6bd988867b0dd
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-01 14:07:45 +0600

    Works elements adding

---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Works = () => {
+    return (
+        <div className='work'>
+            <div className="awesome">
+                <span>My awesome</span>
+                <span>Services</span>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores amet aut aspernatur explicabo deleniti totam possimus nobis, <br /> laudantium praesentium reprehenderit repellendus similique laborum </span>
+
+                <button className='button s-button'>Download CV</button>
+
+                <div className="blur s-blur" style={{ background: '#abf1ff94' }}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Works;
