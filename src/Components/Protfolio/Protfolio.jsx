ba9a2dcf36728735810439ecef00291af8cc6aeb
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-02 16:51:01 +0600

    Swiper gone wrong

--- src/Components/Protfolio/Protfolio.jsx
+++ src/Components/Protfolio/Protfolio.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../Protfolio/Protfolio.css'
+import {swiper, swiperSlide} from 'swiper/react'
+
 
 const Protfolio = () => {
     return (
-        <div>
-            sfge
+        <div className='protfolio'>
+            {/* heading */}
+            <span>Recent projects</span>
+            <span>Protfolio</span>
+
+            {/* slider */}
+           
         </div>
     );
 };
