f87d4799e3e60663117c3e0a8f89464cac27cc5b
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-04 14:07:10 +0600

    Contact form elements added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,8 +3,21 @@ import '../Contact/Contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className='contact-form'>
+            <div className="w-left">
+                <div className="awesome">
+                    <span>Get in touch</span>
+                    <span>Contact me</span>
+                </div>
+            </div>
+            <div className="c-right">
+                <form action="#">
+                    <input type="text" name='user_name' className='user' placeholder='Type your name' />
+                    <input type="email" name='user_email' className='user' placeholder='Type your email' />
+                    <input type="message" name='user_message' className='user' placeholder='Type your message' />
+                    <input type="submit" name='user_name' className='button' value='Send'/>
+                </form>
+            </div>
         </div>
     );
 };
