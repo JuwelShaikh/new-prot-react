78525a2ec9fa85570284f3b7e6cddbf76df74424
JuwelShaikh <juwelshaikh04@gmail.com>
2024-01-07 13:11:34 +0600

    Service motion done

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -8,59 +8,60 @@ import resume from '../Services/Juwel CV PDF.pdf'
 import { motion } from 'framer-motion';
 
 const Services = () => {
-    const transition ={duration:1, type: "sprint"}
+    const transition = { duration: 1, type: "sprint" }
+
     return (
         <div className='services'>
             {/* left-side */}
-        <div className="awesome">
-            <span>My awesome</span>
-            <span>Services</span>
-            <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores amet aut aspernatur explicabo deleniti totam possimus nobis, <br /> laudantium praesentium reprehenderit repellendus similique laborum </span>
-            <a href={resume} download>
-            <button className='button s-button'>Download CV</button>
-            </a>
-            <div className="blur s-blur" style={{background: '#abf1ff94'}}></div>
-        </div>
-        {/* right-side */}
-        <div className="cards">
-            <motion.div
-                initial={{left:"25rem"}}
-                whileInView={{left:'14rem'}}
-                transition={transition}
-            style={{
-                left:'14rem'
-            }}>
-                <Card
-                    emoji={heartEmoji}
-                    heading = {'Desing'}
-                    details ={'Figma, Scratch, Adobe'}
-                />
-            </motion.div>
-            {/* second card */}
-            <motion.div 
-                initial={{right:'24rem'}}
-                whileInView={{right:"15rem"}}
-                transition={transition}
-            style={{top:'12rem', left:'-4rem' }}>
-            <Card
-                    emoji={glasess}
-                    heading = {'Developer'}
-                    details ={'HTML, CSS, JavaScript, React'}
-                />
-            </motion.div>
-            {/* third card */}
-            <motion.div
-                initial={{left:'20rem'}}
-                whileInView={{left:"10rem"}}
-                transition={transition}
-            style={{top:'19rem', left:'12rem' }}>
-            <Card
-                    emoji={humble}
-                    heading = {'UI/UX'}
-                    details ={'lorem ispum text are usually use in section where'}
-                />
-            </motion.div>
-        </div>
+            <div className="awesome">
+                <span>My awesome</span>
+                <span>Services</span>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores amet aut aspernatur explicabo deleniti totam possimus nobis, <br /> laudantium praesentium reprehenderit repellendus similique laborum </span>
+                <a href={resume} download>
+                    <button className='button s-button'>Download CV</button>
+                </a>
+                <div className="blur s-blur" style={{ background: '#abf1ff94' }}></div>
+            </div>
+            {/* right-side */}
+            <div className="cards">
+                <motion.div
+                    initial={{ left: "25rem" }}
+                    whileInView={{ left: '14rem' }}
+                    transition={transition}
+                    style={{
+                        left: '14rem'
+                    }}>
+                    <Card
+                        emoji={heartEmoji}
+                        heading={'Desing'}
+                        details={'Figma, Scratch, Adobe'}
+                    />
+                </motion.div>
+                {/* second card */}
+                <motion.div
+                    initial={{ left: "25rem" }}
+                    whileInView={{ left: '-5rem' }}
+                    transition={transition}
+                    style={{ top: '12rem', left: '-4rem' }}>
+                    <Card
+                        emoji={glasess}
+                        heading={'Developer'}
+                        details={'HTML, CSS, JavaScript, React'}
+                    />
+                </motion.div>
+                {/* third card */}
+                <motion.div
+                    initial={{ left: '20rem' }}
+                    whileInView={{ left: "10rem" }}
+                    transition={transition}
+                    style={{ top: '19rem', left: '12rem' }}>
+                    <Card
+                        emoji={humble}
+                        heading={'UI/UX'}
+                        details={'lorem ispum text are usually use in section where'}
+                    />
+                </motion.div>
+            </div>
         </div>
     );
 };
